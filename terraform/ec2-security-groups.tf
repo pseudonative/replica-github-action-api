f3a7a1f48300e5386ec10bcb97f8783024e935c2
Jeremy Coando <killerpit02@yahoo.com>
2023-10-31 12:00:28 -0600

    terraform plan

---
+++ terraform/ec2-security-groups.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "vpc-ssh" {
+  name        = "vpc-ssh-terragrunt-infracost"
+  description = "Dev VPC SSH"
+
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vpc-ssh-terragrunt-infracost"
+  }
+  provider = aws.us-east-1
+}
+
+resource "aws_security_group" "vpc-web" {
+  name        = "vpc-web-terragrunt-infracost"
+  description = "Dev VPC SSH"
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vpc-web-terragrunt-infracost"
+  }
+  provider = aws.us-east-1
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "Region in which AWS recources to create"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_keypair" {
+  description = "AWS EC2 Keypair"
+  type        = string
+  default     = "terraform-key"
+}
+
+variable "instance_type_list" {
+  description = "EC2 instance type list"
+  type        = list(string)
+  default     = ["t3.micro", "t3.small", "t3.large"]
+}
+
+variable "instance_type_map" {
+  description = "Ec2 instance type map"
+  type        = map(string)
+  default = {
+    "dev"  = "t3.micro"
+    "qa"   = "t3.small"
+    "prod" = "t3.medium"
+  }
+}
+
+variable "Name" {}
