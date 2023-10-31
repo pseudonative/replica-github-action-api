3d3437d957ef14d111f1772c1df86f312c59e89f
Jeremy Coando <killerpit02@yahoo.com>
2023-10-31 13:04:30 -0600

    fixed ec2 error ami image

--- terraform/ami-datasource.tf
+++ terraform/ami-datasource.tf
@@ -1,9 +1,34 @@
+# data "aws_ami" "amzlinux2" {
+#   most_recent = true
+#   owners      = ["amazon"]
+#   filter {
+#     name   = "name"
+#     values = ["al2023-ami-2023.1.20230719.0-kernel-6.1-x86_64"]
+#   }
+#   filter {
+#     name   = "root-device-type"
+#     values = ["ebs"]
+#   }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+#   filter {
+#     name   = "architecture"
+#     values = ["x86_64"]
+#   }
+# }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 data "aws_ami" "amzlinux2" {
   most_recent = true
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.1.20230719.0-kernel-6.1-x86_64"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
   filter {
     name   = "root-device-type"
