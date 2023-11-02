dca7bb94b1b8a49859954f439a4da175b2ecd5d4
Jeremy Coando <killerpit02@yahoo.com>
2023-11-02 08:31:53 -0600

    remove ec2

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -1,19 +1,19 @@
-resource "aws_instance" "myec2vm" {
-  ami           = data.aws_ami.amzlinux2.id
-#   instance_type = var.instance_type
-#   instance_type = var.instance_type_list[1]
-  instance_type = var.instance_type_map["dev"]
-  user_data     = file("${path.module}/app1-install.sh")
-  key_name      = var.instance_keypair
-  vpc_security_group_ids = [
-    aws_security_group.vpc-ssh.id,
-    aws_security_group.vpc-web.id
-  ]
-  count = 1
-  tags = {
-    "Name" = "${var.Name}_Current1_${count.index}"
-    Service = "CICD"
-    Environment = "GithubActionsWorkflow"
-  }
-  provider = aws.us-east-1
-}
+# resource "aws_instance" "myec2vm" {
+#   ami           = data.aws_ami.amzlinux2.id
+# #   instance_type = var.instance_type
+# #   instance_type = var.instance_type_list[1]
+#   instance_type = var.instance_type_map["dev"]
+#   user_data     = file("${path.module}/app1-install.sh")
+#   key_name      = var.instance_keypair
+#   vpc_security_group_ids = [
+#     aws_security_group.vpc-ssh.id,
+#     aws_security_group.vpc-web.id
+#   ]
+#   count = 1
+#   tags = {
+#     "Name" = "${var.Name}_Current1_${count.index}"
+#     Service = "CICD"
+#     Environment = "GithubActionsWorkflow"
+#   }
+#   provider = aws.us-east-1
+# }
