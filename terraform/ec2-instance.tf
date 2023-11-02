83382d9d974ea0e90b922c3a10678f6d44959bc0
Jeremy Coando <killerpit02@yahoo.com>
2023-11-02 09:52:32 -0600

    staging7

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "myec2vm" {
   ]
   count = 1
   tags = {
-    "Name" = "${var.Name}_staging1_${count.index}"
+    "Name" = "${var.Name}_staging7_${count.index}"
     Service = "CICD"
     Environment = "GithubActionsWorkflow"
   }
