76c18c4a81a3a8aeffd012ecdeaa76d822d668c2
Jeremy Coando <killerpit02@yahoo.com>
2023-11-02 10:31:35 -0600

    working workflow

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -17,3 +17,24 @@ resource "aws_instance" "myec2vm" {
   }
   provider = aws.us-east-1
 }
+
+
+output "for_output_map1" {
+  description = "For loop with map"
+  value = {for instance in aws_instance.myec2vm: instance.id => instance.public_dns}
+}
+
+output "for_output_map2" {
+  description = "For loop with map - advanced"
+  value = {for c, instance in aws_instance.myec2vm: c => instance.public_dns}
+}
+
+output "legacy_splat_instance_publicdns" {
+  description = "legacy splat operator"
+  value = aws_instance.myec2vm.*.public_dns
+}
+
+output "latest_splat_instance_publicdns" {
+  description = "latest splat operator"
+  value = aws_instance.myec2vm[*].public_dns
+}
