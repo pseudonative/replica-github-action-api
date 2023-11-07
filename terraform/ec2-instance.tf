1552c3ddf4619f412011f7f1c1ab5072b201ecc4
Jeremy Coando <killerpit02@yahoo.com>
2023-11-07 10:07:50 -0700

    changing instance count

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "myec2vm" {
     aws_security_group.vpc-ssh.id,
     aws_security_group.vpc-web.id
   ]
-  count = 3
+  count = 2
   tags = {
     "Name" = "${var.Name}_QA_${count.index}"
     Service = "CICD"
