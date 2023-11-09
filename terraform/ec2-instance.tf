bf066661b776261396ecea8256c1a418d578b684
Jeremy Coando <killerpit02@yahoo.com>
2023-11-09 15:15:34 -0700

    checking err

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "myec2vm" {
   count = 1
   tags = {
     "Name" = "${var.Name}_v2.1.6_${count.index}"
-    Service = "CICD"
+    Service = "reusableWorkflow"
     Environment = "GithubActionsWorkflow"
   }
   provider = aws.us-east-1
