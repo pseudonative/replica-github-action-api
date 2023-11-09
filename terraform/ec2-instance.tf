6903e2918c2cb051af277848c1f5e04838c1829c
Jeremy Coando <killerpit02@yahoo.com>
2023-11-09 15:05:19 -0700

    weird

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "myec2vm" {
   ]
   count = 1
   tags = {
-    "Name" = "${var.Name}_v2.1.5_${count.index}"
+    "Name" = "${var.Name}_v2.1.6_${count.index}"
     Service = "CICD"
     Environment = "GithubActionsWorkflow"
   }
